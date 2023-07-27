916bc355b0fb798536773f701b09923c8335e3c6
priyankaaurade1 <70333636+priyankaaurade1@users.noreply.github.com>
2023-07-27 19:03:05 +0100

    27/07/23

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     # index
-    path('',views.index, name='index'),
+    path('index/',views.index, name='index'),
+    path('pages_error_404/',views.pages_error_404, name='pages_error_404'),
+    path('pages_contact/',views.pages_contact, name='pages_contact'),
     path('user_profile/',views.user_profile, name='user_profile'),
+    path('pages_faq/',views.pages_faq, name='pages_faq'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,12 @@ def index(request):
 
 def user_profile(request):
     return render(request, 'users-profile.html')
+
+def pages_error_404(request):
+    return render(request, 'pages-error-404.html')
+
+def pages_contact(request):
+    return render(request, 'pages-contact.html')
+
+def pages_faq(request):
+    return render(request, 'pages-faq.html')

c9292f5caf50f5927dd10af9025310c1e17aa99e
Forms9 <65807794+sarvesh0809@users.noreply.github.com>
2023-08-07 14:45:19 +0530

    first commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('pages_contact/',views.pages_contact, name='pages_contact'),
     path('user_profile/',views.user_profile, name='user_profile'),
     path('pages_faq/',views.pages_faq, name='pages_faq'),
-    path('driver_details/', views.driver_details, name='driver_details'),
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -17,26 +17,4 @@ def pages_contact(request):
 def pages_faq(request):
     return render(request, 'partials/pages-faq.html')
 
-# driver_details
-def driver_details(request):
-    if request.method == 'POST':
-        form = DriverForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = DriverForm()
-    return render(request, 'driver/driver_details.html', {'form': form})
 
-def get_latest_driver_data():
-    latest_driver = Driver.objects.last()
-    print(latest_driver)
-
-    if latest_driver:
-        return {
-            'driver_name': latest_driver.driver_name,
-            # 'driver_email': latest_driver.driver_email,
-            'driver_license_number': latest_driver.driver_license_number,
-            'driver_contact_number': latest_driver.driver_contact_number,
-            'driver_address': latest_driver.driver_address,
-        }
-    return {}

5497cb12158840a8719617f9d7f4a3be56983832
Forms9 <65807794+sarvesh0809@users.noreply.github.com>
2023-08-07 14:51:29 +0530

    check

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .forms import DriverForm
-from .models import Driver
 def index(request):
     return render(request, 'index.html')
 
